6741252d82da210abb4927fe415fa32e3dd4a951
italo <italokaualima100@gmail.com>
2026-07-25 21:01:25 -0300

    refactor: atualiza interface do jokenpo e regras da forca com colorama

--- Jogos/jogo_forca.py
+++ Jogos/jogo_forca.py
@@ -1,5 +1,12 @@
 from random import choice
+from colorama import Fore, Style, init
 
+separador = '=' * 45
+
+# Inicialização do colorama
+init(autoreset=True)
+
+# Palavras 
 palavras = ['PSICOLOGIA', 'PARADOXO', 'GIRASSOL', 'MOCHILA', 'ALGORITMO',
             'TERMINAL', 'XADREZ', 'PYTHON']
 
@@ -8,21 +15,47 @@ acertos = ['_'] * len(escolha)
 
 tentativas = 0
 
-while tentativas < 3:
+# Lista de letras repetidas
+repetidas = []
 
+# Título
+print(separador)
+print(Fore.CYAN + 'JOGO DA FORCA'.center(20))
+print(separador)
+
+while True:
     letra = input('Letra: ').upper().strip()
 
-    if letra not in escolha:
+    # Tratamento de erros de número
+    if letra.isnumeric():
+        continue
+    
+    # Tratamento de letras repetidas
+    if letra in repetidas:
+        print(Fore.LIGHTYELLOW_EX + 'Você já tentou essa letra')
+        continue
+    else:
+        repetidas.append(letra)
+
+    # Análise das letras
+    if letra in escolha:
+        for p, v in enumerate(escolha):
+            if letra == v:
+                acertos[p] = v
+    else:
+        print(f'Não tem {Fore.RED + letra}')
         tentativas += 1
-
-    for p, v in enumerate(escolha):
-        if letra == v:
-            acertos[p] = v
-
     for i in acertos:
-        print(i, end=' ')
+        print(Fore.GREEN + i if i != '_' else i, end=' ')
     print()
 
+    # Mensagens de resultado
     if acertos == escolha:
-        print('Você ganhou')
+        print(separador)
+        print(Fore.GREEN + '🎉 VOCÊ GANHOU! 🎉')
+        break
+    if tentativas == 4:
+        print(separador)
+        print(Fore.RED + '❌ VOCÊ PERDEU! ❌')
+        print(f'A palavra era {Fore.CYAN + "".join(escolha)}')
         break
--- Jogos/jokenpo.py
+++ Jogos/jokenpo.py
@@ -1,22 +1,80 @@
 from random import choice
+from colorama import Fore, Style, init
+from time import sleep
+
+# Inicialização do Colorama
+init(autoreset=True)
+
+# Divisória estilizada em Ciano Escuro
+separador = Fore.CYAN + '=' * 50
+
+# Título Principal
+print(f'\n{separador}\n{Style.BRIGHT + Fore.YELLOW + "🎮 PEDRA, PAPEL E TESOURA 🎮"}\n{separador}')
+
+valores = ['pedra', 'papel', 'tesoura']
 
 while True:
-    jogador = input('Escolha pedra, papel ou tesoura: ').lower().strip()
-    maquina = choice(['pedra', 'papel', 'tesoura'])
- 
-    print(f'Você escolheu: {jogador} e a Maquina escolheu: {maquina}')
-    if maquina == jogador:
-        print('Empate!')
-    elif (
-        (jogador == 'papel' and maquina == 'pedra') or
-        (jogador == 'pedra' and maquina == 'tesoura') or
-        (jogador == 'tesoura' and maquina == 'papel')
-    ):
-        print('Você venceu!')
-    else:
-        print('Você perdeu!')
+    # Menu Principal
+    print(f'''
+{Style.BRIGHT + Fore.CYAN}--- MENU PRINCIPAL ---{Style.RESET_ALL}
 
-    continuar = input('Deseja continuar? (S/N) ').lower().strip()[0]
-    if continuar == 'n':
-        print('Valeu por jogar, volte sempre!')
+{Fore.WHITE}[ 1 ] {Fore.GREEN}🎮 Iniciar o jogo
+{Fore.WHITE}[ 2 ] {Fore.BLUE}📕 Como jogar?
+{Fore.WHITE}[ 3 ] {Fore.RED}🚪 Sair  
+''')
+    
+    resp = int(input('Escolha: '))
+    print()
+    print(separador)
+
+    if resp == 1:
+        while True:
+
+            maquina = choice(valores)
+
+            print(f'''\n{Fore.WHITE}[ 1 ] 🪨  Pedra
+[ 2 ] ✋ Papel
+[ 3 ] ✂️  Tesoura''')
+            jogador = input('Escolha Pedra, Papel ou Tesoura: ').lower().strip()
+
+            print(separador)
+
+            if jogador.isnumeric():
+                jogador = int(jogador)
+                jogador = valores[jogador - 1]
+
+            # Exibição de escolhas com cores em destaque
+            print(f'Você: {Fore.CYAN + Style.BRIGHT + jogador.capitalize()} {Fore.WHITE}| Maquina: {Fore.MAGENTA + Style.BRIGHT + maquina.capitalize()}\n')
+            
+            # Mensagens de resultado
+            if maquina == jogador:
+                print(Fore.YELLOW + Style.BRIGHT + '🤝 Empate!')
+            elif (
+                (jogador == 'papel' and maquina == 'pedra') or
+                (jogador == 'pedra' and maquina == 'tesoura') or
+                (jogador == 'tesoura' and maquina == 'papel')
+            ):
+                print(Fore.GREEN + Style.BRIGHT + '🎉 VOCÊ VENCEU! 🎉')
+            else:
+                print(Fore.RED + Style.BRIGHT + '❌ VOCÊ PERDEU! ❌')
+
+            print(separador)
+            break
+
+    elif resp == 2:
+
+        # Tela de regras padronizada com o estilo do jogo
+        print(Style.BRIGHT + Fore.CYAN + "📜 REGRAS DO JOKENPÔ\n")
+        print(f"{Fore.WHITE}- Pedra ganha da Tesoura e perde pro Papel")
+        print(f"{Fore.WHITE}- Papel ganha da Pedra e perde pra Tesoura")
+        print(f"{Fore.WHITE}- Tesoura ganha do Papel e perde pra Pedra")
+        print(Fore.YELLOW + "- Escolhas iguais = Empate!")
+        print(separador)
+
+    elif resp == 3:
+        print(Fore.YELLOW + 'Saindo...')
+        sleep(1)
+        print(Fore.GREEN + 'Volte sempre! 👋')
         break
+    else:
+        print(Fore.RED + 'Opção inválida, tente novamente!')
